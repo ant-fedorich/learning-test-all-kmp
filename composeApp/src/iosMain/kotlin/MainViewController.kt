39ce0e45cd58a63e81c37d01ce229b14c99b6646
eltonio <eltonio@com.com>
2024-07-10 15:13:40 +0300

    add SwiftUI components

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,10 +1,60 @@
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.interop.UIKitViewController
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
-import temp.IOSClass
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.UIKit.UIViewController
 
 fun MainViewController() = ComposeUIViewController {
     MaterialTheme {
-        MainScreenShared()
+//        MainScreen()
+//        MainScreenShared()
+        Box(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.size(50.dp))
+
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {},
+            ) {
+                Text("Click me please")
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalForeignApi::class)
+fun ComposeEntryPointWithUIViewController(
+    uiViewController: () -> UIViewController
+) = ComposeUIViewController {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.systemBars),
+    ) {
+        Text("How to use SwiftUI inside Compose Multiplatform")
+        UIKitViewController(
+            modifier = Modifier.size(800.dp).padding(16.dp),
+            factory = uiViewController,
+        )
     }
 }
 
--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,58 +3,91 @@ import SwiftUI
 import ComposeApp
 
 struct ComposeView: UIViewControllerRepresentable {
+    @State var celsius: Double = 0
+    @State var name: String = ""
+    @State var isOn: Bool = false
+    @State var value: Double = 0.5
+    @State var selection: String = "Option 1"
+    @State private var stepValue: Int = 1
+    @State private var selectedValue: String = "One"
+      let values = ["One", "Two", "Three"]
+
+
+
+
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+//        MainViewControllerKt.MainViewController()
+
+        MainViewControllerKt.ComposeEntryPointWithUIViewController {
+            let swiftUIView = VStack {
+                Text("SwiftUI in Compose Multiplatform").font(.title2)
+                
+                Spacer().frame(height: 20)
+                
+                HStack {
+                    Button(action: {}) {
+                        Text("Button1")
+                    }
+                
+                    Button(action: {}) {
+                        Text("Button2")
+                    }.buttonStyle(BorderedButtonStyle())
+                                  
+                    Button(action: {}) {
+                        Text("Button3")
+                    }.buttonStyle(BorderedProminentButtonStyle())
+                }
+
+    
+                Spacer().frame(height: 20)
+                TextField("Enter your name", text: $name)
+                    .padding()
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.gray, lineWidth: 1)
+                    )
+                    .padding()
+                
+                Toggle("Enable feature", isOn: $isOn).padding()
+                Spacer().frame(height: 20)
+                
+                Stepper("Value: \(stepValue)", value: $stepValue, in: 0...10)
+                    .padding()
+                
+                Picker("Select a value", selection: $selectedValue) {
+                    ForEach(values, id: \.self) { value in
+                        Text(value)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding()
+                
+                Slider(value: $value, in: 0...1)
+
+                
+                
+                let options = ["Option 1", "Option 2", "Option 3"]
+                Picker("Select an option", selection: $selection) {
+                    ForEach(options, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .padding()
+                
+                
+                Spacer().frame(height: 20)
+                
+            }
+            return UIHostingController(rootView: swiftUIView)
+        }
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
+
 struct ContentView: View {
     var body: some View {
-        ComposeView()
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+        ComposeView().ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
-
-//
-////added
-//class ComposeUIViewController: UIViewController {
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        let composeView = ComposeView()
-//        composeView.setContent {
-//            CommonButton(text: "Click Me", onClick = {
-//                // Handle button click
-//            })
-//        }
-//
-//        view = composeView
-//    }
-//}
-//
-//
-////Added
-//@UIApplicationMain
-//class AppDelegate: UIResponder, UIApplicationDelegate {
-//
-//    var window: UIWindow?
-//
-//    func application(
-//        _ application: UIApplication,
-//        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-//    ) -> Bool {
-//        window = UIWindow(frame: UIScreen.main.bounds)
-//
-//        let composeViewController = ComposeUIViewController()
-//
-//        window?.rootViewController = composeViewController
-//        window?.makeKeyAndVisible()
-//
-//        return true
-//    }
-//}
-
-
-
